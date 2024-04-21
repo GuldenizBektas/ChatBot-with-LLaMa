81e41b805a5af53b3a163a50c0c457ada6ff9ede
GuldenizBektas <gdenizbektass@gmail.com>
2024-04-21 15:11:49 +0300

    Change variable names etc.

--- collect_data.py
+++ collect_data.py
@@ -1,11 +1,12 @@
 import arxiv
 
 # papers about LLMs
-paper_ids = ['2308.10620', '2307.06435', '2303.18223', '2307.10700', '2310.11207', '2305.11828']
+# paper_ids = ['2308.10620', '2307.06435', '2303.18223', '2307.10700', '2310.11207', '2305.11828']
+paper_ids = ['1905.11946', '1706.03762', '1907.11692', '2305.10435']
 
 for paper_id in paper_ids:
     search = arxiv.Search(id_list=[paper_id])
     paper = next(search.results())
     print(paper.title)
 
-    paper.download_pdf("data/")
+    paper.download_pdf("data2/")
--- store_vector_space.py
+++ store_vector_space.py
@@ -1,10 +1,17 @@
 from langchain.document_loaders import DirectoryLoader, TextLoader, PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
+from dotenv import load_dotenv
+import os 
+
+load_dotenv()
+
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 
 # define what documents to load
-loader = DirectoryLoader(path='data', glob="*.pdf", loader_cls=PyPDFLoader)
+loader = DirectoryLoader(path='data2', glob="*.pdf", loader_cls=PyPDFLoader)
 
 # interpret information in the documents
 documents = loader.load()
@@ -12,14 +19,16 @@ splitter = RecursiveCharacterTextSplitter(chunk_size=500,
                                           chunk_overlap=50)
 texts = splitter.split_documents(documents)
 
-embeddings = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2",
-    model_kwargs={'device': 'cpu'})
+# embeddings = HuggingFaceEmbeddings(
+#     model_name="sentence-transformers/all-MiniLM-L6-v2",
+#     model_kwargs={'device': 'cpu'})
+
+embeddings = OpenAIEmbeddings()
 
 # create the vector store database
 db = FAISS.from_documents(texts, embeddings)
 
 # save the vector store
-db.save_local("faiss")
+db.save_local("faiss2")
 
-print("DONE SAVING!)")
+print("DONE SAVING!")
